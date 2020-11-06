d50873871dd1a9e50399f5f2bbf8dbb9b2a8c812
tudte <2623424675@qq.com>
2020-11-06 21:59:47 +0800

    22

---
+++ src/main/java/com/lxy/tc/config/MVCConfig.java
@@ -0,0 +1,10 @@
+package com.lxy.tc.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @Author tudte
+ * @Date 2020/11/6 21:59
+ */
+public class MVCConfig extends WebMvcConfigurationSupport {
+}
